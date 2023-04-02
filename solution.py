7d55dbae7dc2aefea4ceb0f23bd5c8073218c618
Khachatur <khachkara@gmail.com>
2023-04-02 23:22:01 +0300

    solution test2
    Карапетян Хачатур

--- solution.py
+++ solution.py
@@ -1,17 +1,16 @@
 import pandas as pd
+from scipy import stats
 import numpy as np
 
 from scipy.stats import norm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    b_ml = np.max(x)
+    s = np.std(x, ddof=1)
+    t = stats.t.ppf((1+p)/2, n-1)
+    delta = t * s / np.sqrt(n)
+    b_interval = (max(b_ml, b_ml - delta), b_ml + delta)
+    return  b_interval
