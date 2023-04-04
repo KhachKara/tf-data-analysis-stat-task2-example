f9193605e516f88396bcd3d2f3909caa3ee06a76
Khachatur <khachkara@gmail.com>
2023-04-04 21:39:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,18 @@
 import pandas as pd
-from scipy import stats
 import numpy as np
-
+from scipy.stats import chi2
 from scipy.stats import norm
 
 chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    b_ml = np.max(x)
-    s = np.std(x, ddof=1)
-    t = stats.t.ppf((1+p)/2, n-1)
-    delta = t * s / np.sqrt(n)
-    b_interval = (max(b_ml, b_ml - delta), b_ml + delta)
-    return  b_interval
+    alpha = 1 - p
+    df = n - 1
+    var = np.var(x, ddof=1)
+    lower_crit = chi2.ppf(alpha / 2, df)
+    upper_crit = chi2.ppf(1 - alpha / 2, df)
+    var1 = df * var / upper_crit
+    var2 = df * var / lower_crit
+    b1 = (var1 * 12)**0.5 + 0.095
+    b2 = (var2 * 12)**0.5 + 0.095
+    return b1, b2
