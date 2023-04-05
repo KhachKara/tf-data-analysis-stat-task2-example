40b51068d2429119f841523a890436ac044a6bb9
Khachatur <khachkara@gmail.com>
2023-04-05 13:52:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,9 @@
-import pandas as pd
-import numpy as np
-from scipy.stats import chi2
-from scipy.stats import norm
-
-chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
-
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    n = len(x)
-    df = n - 1
-    var = np.var(x, ddof=1)
-    lower_crit = chi2.ppf(alpha / 2, df)
-    upper_crit = chi2.ppf(1 - alpha / 2, df)
-    var1 = df * var / upper_crit
-    var2 = df * var / lower_crit
-    b1 = (var1 * 12)**0.5 + 0.095
-    b2 = (var2 * 12)**0.5 + 0.095
-    return b1, b2
+    alpha1 = (1 - p) / 2
+    alpha2 = (1 + p) / 2
+    n = x.size
+    a1, a2 = pow(alpha1, 1 / n), pow(alpha2, 1 / n)
+
+    x_max = x.max()
+    return (x_max - 0.095) / a2 + 0.095, \
+           (x_max - 0.095) / a1 + 0.095
